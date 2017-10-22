d0fa4e54bd559b59de018cbc0103aed5705a7e2c
Willian Werner <willianwernerangelodacosta@gmail.com>
2017-10-22 12:32:11 -0200

    Ampliação dos campos registrados

--- crawlers/spiders/economic_freedom.py
+++ crawlers/spiders/economic_freedom.py
@@ -2,22 +2,47 @@
 import scrapy, json, pdb
 from scrapy.selector import Selector
 
-field_list=['name', 'efi']
+field_list=[
+    u'name',
+    u'index year',
+    u'overall score',
+    u'property rights',
+    u'government integrity',
+    u'judicial effectiveness',
+    u'tax burden',
+    u'government spending',
+    u'fiscal health',
+    u'business freedom',
+    u'labor freedom',
+    u'monetary freedom',
+    u'trade freedom',
+    u'investment freedom',
+    u'financial freedom',
+]
 
 class EconomicFreedomSpider(scrapy.Spider):
     name = 'economic_freedom'
     allowed_domains = []
     start_urls = ['http://www.heritage.org/index/explore']
-
+    custom_settings = {
+        'FEED_FORMAT':'csv',
+        'FEED_URI': 'output/economic_freedom.csv',
+    }
+    
     def parse(self, response):
-	hxs = Selector(response)
-	pdb.set_trace()
-	for country in hxs.xpath("//table[@id='ctl00_cphContent_ExpMulti_GridView1']/tbody/tr"):
-		item =  EconomicFreedomSpiderItem()
-		item['name'] = country.xpath("td[@class='explore-country']/a/text()").extract_first()
-		item['efi'] = country.xpath("td/span[contains(@id, 'lblYearScore')]/text()").extract_first()
-		
-		yield item
+        items = {}
+        table = Selector(response).xpath("//table[@id='ctl00_cphContent_ExpMulti_GridView1']")
+        fields = table.xpath("thead/tr/th/text()").extract()
+        for i in range(1, len(fields)):
+            field = fields[i]
+            for item in table.xpath("tbody/tr"):
+                name = item.xpath("td[1]/a/text()").extract_first()
+                if not name in items:
+                    items[name] = EconomicFreedomSpiderItem()
+                    items[name]['name'] = name
+                items[name][field] = item.xpath("td[%d]/span/text()" % (i + 1)).extract_first()
+        for item in items.values():
+            yield item
         
 
 class EconomicFreedomSpiderItem(scrapy.Item):
